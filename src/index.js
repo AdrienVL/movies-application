d7c8414cca3f6e1a56f6e0865a14c80506a67996
AdrienVL <adrienlhemann@gmail.com>
2019-03-18 23:44:13 -0500

    Finished Loading functionality

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-/**
- * es6 modules and imports
- */
-import sayHello from './hello';
-sayHello('World');
+const $ = require('jquery');
 
-/**
- * require style imports
- */
+
+
+
 const {getMovies} = require('./api.js');
+
+
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
+
+
+
+    $('.loading').remove();
+    $('.container').append('<div>' + title + '</div>');
+
+
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+
+
+
+//Loading functionality- Display loading function at the start on HTML. Remove class when the page has loaded
