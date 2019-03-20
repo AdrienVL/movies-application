d8b6fe86d3535d0aca0df50dbea72e49afa6d712
AdrienVL <adrienlhemann@gmail.com>
2019-03-19 20:54:05 -0500

    Completed delete functionality

--- src/index.js
+++ src/index.js
@@ -2,81 +2,94 @@ const $ = require('jquery');
 //Modules link with api
 const {getMovies} = require('./api.js');
 
+function disPlayMovies(){
+
+
+    $('.container').html("");
+    getMovies().then((movies) => {
+
+
+
+        movies.forEach(({title, rating, id}) => {
+            console.log(`id#${id} - ${title} - rating: ${rating}`);
+
+
+            $('.loading').remove();
+            $('.container').append("<div id='" + id + "'>" +
+                'id: ' + id + ', ' +
+                'title: ' + title + ', ' +
+                'rating: ' + rating +
+                '</div>');
+
+
+//Store the id of the movie to be deleted. Click on the delete button to get rid of movie from display.
+            $('#' + id).click(function () {
+                console.log(id);
+                $('#delete').click(function () {
+
+                    console.log('/api/movies/' + id);
+                    const options = {
+                        method: 'DELETE',
+
+                    };
+                    fetch('/api/movies/' + id, options)
+                        .then(response => console.log(response))
+                        .catch(error => console.log(error));
+
+                    disPlayMovies();
+
+                })
+            })
+
+        });
+
+
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.')
+        console.log(error);
+    });
+
+    // Infinity Loop
+    // disPlayMovies();
+
+}
+
 //-----------------------------------------------------------------------------------------------------Add Movie--------------------------------------------------------------------------//
-function addMovie(){
+function addMovie() {
 
 
-  const movieTitleRatings = {title: $('#title').val(), rating: $('#rating').val()};
-  const url = '/api/movies';
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(movieTitleRatings),
-  };
-  fetch(url, options)
-      .then(response => console.log(response))
-      .catch(error =>console.log(error));
-
-}
-
-function deleteMovie(id){
+    const movieTitleRatings = {title: $('#title').val(), rating: $('#rating').val()};
+    const url = '/api/movies';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(movieTitleRatings),
+    };
+    fetch(url, options)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
 
 
-  const options = {
-    method: 'DELETE',
 
-  };
-  fetch('/api/movies/', options)
-      .then(response => console.log(response))
-      .catch(error =>console.log(error));
+    disPlayMovies();
 
 }
+
 
 //-------------------------------------------------------------------------------------------------Displaying Movie------------------------------------------------------------------------//
 
-getMovies().then((movies) => {
 
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+disPlayMovies();
 
-
-
-    $('.loading').remove();
-    $('.container').append("<div id='"+id+"'>" +
-        'id: ' + id + ', ' +
-        'title: ' + title + ', ' +
-        'rating: ' + rating +
-        '</div>');
-
-
-
-    $('#'+id).click(function(){
-      console.log(id);
-      $('#delete').click(function(){
-        console.log(id);
-        deleteMovie(id);
-
-      })
-    })
-
-  });
-
-
-
-
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
 
 
 //------------------------------------------------------------------------------------------------Buttons--------------------------------------------------------------------------------//
 
-$('#submit').click(function(){
+$('#submit').click(function () {
 
-  return addMovie();
+    return addMovie();
 });
 
 
